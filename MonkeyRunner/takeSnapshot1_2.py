7ea0adfe744625f7f7d45a1c9affc3e69d9fbc52
zhanglinping <18701158269@163.com>
2017-04-12 16:18:28 +0800

    add cal test

---
+++ MonkeyRunner/takeSnapshot1_2.py
@@ -0,0 +1,20 @@
+﻿import sys
+from com.android.monkeyrunner import MonkeyRunner as mr
+from com.android.monkeyrunner import MonkeyDevice as md
+from com.android.monkeyrunner import MonkeyImage as mi
+
+device = mr.waitForConnection(1.0,'emulator-5554')
+device2 = mr.waitForConnection(1.0,'emulator-5556')
+if not device:
+    print >> sys.stderr,"fail"
+    sys.exit(1)
+componentName='kg.monkey/.MonkeyActivity'
+device.startActivity(component=componentName)
+mr.sleep(3.0)
+device.type('asd')
+device.press('KEYCODE_ENTER')
+mr.sleep(3.0)
+result = device.takeSnapshot()
+result2 = device2.takeSnapshot()
+result.writeToFile('takeSnapshot\\result1.png','png');
+result2.writeToFile('takeSnapshot\\result2.png','png');
---
+++ MonkeyRunner/takescreen.py
@@ -0,0 +1,18 @@
+import sys
+from com.android.monkeyrunner import MonkeyRunner as mr
+from com.android.monkeyrunner import MonkeyDevice as md
+from com.android.monkeyrunner import MonkeyImage as mi
+
+device = mr.waitForConnection()
+if not device:
+    print >> sys.stderr,"fail"
+    sys.exit(1)
+componentName='com.android.calculator2/com.android.calculator2.Calculator'
+device.startActivity(component=componentName)
+mr.sleep(3.0)
+device.type('asd')
+device.press('KEYCODE_ENTER')
+mr.sleep(3.0)
+result = device.takeSnapshot()
+
+result.writeToFile('resul1t\\result1.png','png');
