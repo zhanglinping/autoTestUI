eaa88beaec6dfa17a135650a73e40cd75bbae4a5
zhanglinping <18701158269@163.com>
2017-04-26 10:37:56 +0800

    add uiautomator

---
+++ GuiTest/runGuiTestInFold.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xu
+ip_android=192.168.0.161
+adbPort=5555
+testfold=testcases
+date=`date +%Y%m%d`
+
+mkdir $date
+adb kill-server
+adb connect $ip_android
+
+./buildGuiTestcase.sh
+cd $testfold
+for testcase in `ls -d */|sed 's|[/]||g'`
+do  
+    $testcase/$testcase".sh" $ip_android $adbPort $testcase"_result"
+    mv $testcase/$testcase"_result" ../$date
+done
