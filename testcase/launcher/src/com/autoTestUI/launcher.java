7765b2790a7ba10c220f4c1f9f1f9c4ae103e6ca
zhanglinping <18701158269@163.com>
2017-10-13 10:26:59 +0800

    add some new testcase

--- testcase/launcher/src/com/autoTestUI/launcher.java
+++ testcase/launcher/src/com/autoTestUI/launcher.java
@@ -61,6 +61,8 @@ public class launcher extends UiAutomatorTestCase {
 		UiObject newxls = new UiObject(new UiSelector().text("新建文件1.xls"));
 		assertTrue("新建Excel表格文档",newxls.exists());
 		
+		lch.click();
+		sleep(500);
 		lch.longClick();
 		otoTest.ClickByText("新建文件");
 		otoTest.ClickById("com.android.launcher3:id/cancel");
@@ -167,10 +169,14 @@ public class launcher extends UiAutomatorTestCase {
 		UiObject Uzip =new UiObject(new UiSelector().text("usercomp.zip"));
 		assertTrue("压缩zip",Uzip.exists());
 		//压缩新建文件夹为.7z,并设置密码123
+		lch.click();
+		sleep(500);
 		newfolder.longClick();
 		otoTest.ClickByText("压缩");
 		otoTest.ClickById("com.openthos.compress:id/bt_co_destination");
 		sleep(500);
+		UiObject bf = new UiObject(new UiSelector().resourceId("com.openthos.compress:id/lv_file_list")).getChild(new UiSelector().className("android.widget.RelativeLayout").instance(9));
+		bf.dragTo(0,100,40);
 		otoTest.ClickByText("Desktop");
 		sleep(500);
 		otoTest.ClickByText("新建文件夹1");
@@ -205,6 +211,7 @@ public class launcher extends UiAutomatorTestCase {
 		sleep(2000);
 		assertTrue("剪切txt文件",newtxt.exists());
 		//复制doc
+		lch.click();
 		newdoc.longClick();
 		otoTest.ClickByText("复制");
 		lch.longClick();
@@ -271,6 +278,7 @@ public class launcher extends UiAutomatorTestCase {
 		otoTest.ClickByText("删除");
 		otoTest.ClickById("android:id/button1");
 		sleep(1000);
+		lch.click();
 		newfolder.longClick();
 		otoTest.ClickByText("删除");
 		otoTest.ClickById("android:id/button1");
